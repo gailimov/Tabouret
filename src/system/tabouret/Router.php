b78a110bf53db035027e7932370dc8c608d469df
Vis-a-vis <gailimov@gmail.com>
2012-03-27 12:05:41 +0500

    Added license information

--- src/system/tabouret/Router.php
+++ src/system/tabouret/Router.php
@@ -1,10 +1,15 @@
 <?php
 
 /**
- * Tabouret
+ * Tabouret — Lightweight PHP Framework
+ * "Simple as a tabouret"
+ *
+ * For the license information, please view the LICENSE file
+ * that was distributed with this source code.
  *
  * @author    Kanat Gailimov <gailimov@gmail.com>
  * @copyright Copyright (c) 2012 Kanat Gailimov (http://kanat.gailimov.kz)
+ * @license   http://www.opensource.org/licenses/mit-license.html MIT License
  */
 
 namespace tabouret;
